e8aaf09f7ba911920acbd93fa48fe1f7c91767a4
Kopasz98 <kozmabalazs9813@gmail.com>
2025-12-16 21:26:04 +0100

    Update rss_to_discord.py

--- rss_to_discord.py
+++ rss_to_discord.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import feedparser
 import requests
 import json
@@ -71,7 +72,7 @@ INCLUDE_KEYWORDS = [k.lower() for k in [
     "Epic Games","Respawn Entertainment","Fallout","Game Awards","GTA","Hello Games","io interactive","FromSoftware","Krafton",
     "Leslie Benzies","MindsEye","No Man's Sky","Overwatch","PlayerUnknown's Battlegrounds","Polyphony Digital","Rainbow Six Siege",
     "R6S","Raven Software","ready or not","Schedule 1","Sims","Snail Games","ARK","Techland","tekken","Tom Clancy's Rainbow Six Siege X",
-    "TVGS","Tyler's Video Game Studio","void interactive","EA Sports FC"
+    "TVGS","Tyler's Video Game Studio","void interactive","EA Sports FC","Tomb Raider"
 ]]
 
 EXCLUDE_KEYWORDS = [k.lower() for k in [
